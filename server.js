4085e9e5f7bba3f851c08000c7d67472c856370c
Steve Rayner <srayner02@gmail.com>
2018-09-19 22:20:11 +0100

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 const port = 8000;
 const app = express();
+
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+
 app.use(bodyParser.json());
+app.use(allowCrossDomain);
 
 MongoClient.connect(
   "mongodb://localhost:27017/zeedle",
