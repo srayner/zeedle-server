63987b3d4cd005490805672b73b52432b2e418b5
Steve Rayner <srayner02@gmail.com>
2018-10-20 10:59:23 +0100

    Added task-model.js

--- app/model/task-model.js
+++ app/model/task-model.js
@@ -4,4 +4,8 @@ var taskSchema = new mongoose.Schema({
   title: { type: String, required: true }
 });
 
+taskSchema.set("toJSON", {
+  virtuals: true
+});
+
 exports.Task = mongoose.model("task", taskSchema);
