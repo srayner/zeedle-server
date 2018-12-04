5bab5eefd41496eea763c3ed17c7fd190a10c7e5
Steve Rayner <srayner02@gmail.com>
2018-12-04 21:07:09 +0000

    allow filter columns by boardId

--- app/routes/column-routes.js
+++ app/routes/column-routes.js
@@ -3,15 +3,43 @@ var ObjectID = require("mongodb").ObjectID;
 module.exports = function(app, db) {
   // INDEX Columns
   app.get("/columns", (req, res) => {
-    db.collection("columns")
-      .find({})
-      .toArray(function(err, result) {
-        if (err) {
-          res.send({ error: "An error occurred" });
-        } else {
-          res.send(result);
+    if (req.query.hasOwnProperty("boardId")) {
+      db.collection("boards").findOne(
+        { _id: new ObjectID(req.query.boardId) },
+        function(err, board) {
+          if (err) {
+            res.send({ error: "An error occured" });
+          } else {
+            if (board !== null) {
+              const objectIds = board.columnIds.map(function(id) {
+                return new ObjectID(id);
+              });
+              db.collection("columns")
+                .find({ _id: { $in: objectIds } })
+                .toArray(function(err, result) {
+                  if (err) {
+                    res.send({ error: "An error occurred" });
+                  } else {
+                    res.send(result);
+                  }
+                });
+            } else {
+              res.send("Board not found");
+            }
+          }
         }
-      });
+      );
+    } else {
+      db.collection("columns")
+        .find({})
+        .toArray(function(err, result) {
+          if (err) {
+            res.send({ error: "An error occurred" });
+          } else {
+            res.send(result);
+          }
+        });
+    }
   });
 
   // CREATE Column
