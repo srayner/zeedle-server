c25eb5f818bc6a5d4b9f5d2d6d0be1fbb5ad0043
Steve Rayner <srayner02@gmail.com>
2018-12-13 08:04:14 +0000

    completed user signup

--- app/routes/user-routes.js
+++ app/routes/user-routes.js
@@ -5,25 +5,36 @@ const User = require("../model/user");
 module.exports = function(app, db) {
   // USER signup
   app.post("/user/signup", (req, res) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-      if (err) {
-        return res.status(500).json({ error: err });
-      } else {
-        const user = new User({
-          _id: new mongoose.Types.ObjectId(),
-          email: req.body.email,
-          password: hash
-        });
-        user
-          .save()
-          .then(result => {
-            res.status(201).json({ message: "User created." });
-          })
-          .catch(err => {
-            res.status(500).json({ error: err });
+    User.find({ email: req.body.email })
+      .exec()
+      .then(user => {
+        if (user.length >= 1) {
+          return res
+            .status(409)
+            .json({ message: "Email address already taken." });
+        } else {
+          bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) {
+              return res.status(500).json({ error: err });
+            } else {
+              const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash
+              });
+              user
+                .save()
+                .then(result => {
+                  res.status(201).json({ message: "User created." });
+                })
+                .catch(err => {
+                  res.status(500).json({ error: err });
+                });
+            }
           });
-      }
-    });
+        }
+      })
+      .catch();
   });
 
   // USER sign in
